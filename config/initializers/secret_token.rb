37c7e0d883fbfab1ad87870f6d96ebbceca8b02d
Brayden Cleary <brayden.cleary@gmail.com>
2013-10-30 18:03:46 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaceWithMe::Application.config.secret_token = 'ae5e324ea853b3e0edca551012beeb0f787880fea84212916f0d3f5d2add28ed35599f6271e94ce910b7a3af7a5ebcf698fbc2ff65672714f6e1a2d1751973d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RaceWithMe::Application.config.session_store :cookie_store, key: '_race_with_me_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RaceWithMe::Application.config.session_store :active_record_store
